6d002dad126c91bdf8b46980294e518ee5fcd4be
Usha/Gavri <usha/gavri@example.com>
2011-05-27 09:18:04 +0530

    Second

--- InstantRunOff.hs
+++ InstantRunOff.hs
@@ -1,26 +1,34 @@
 module InstantRunOff where
 import Data.List
 import Data.Function
+import Data.Functor
+import Data.Maybe
+import Control.Monad
+import Control.Applicative
+import Debug.Trace
 
 vm = transpose [
- [1, 3, 1, 1, 2],
- [2, 2, 2, 3, 1],
- [3, 1, 3, 2, 1]
+ [1, 1, 2, 2, 3, 3],
+ [2, 2, 3, 3, 1, 1],
+ [3, 3, 1, 1, 2, 2]
  ]
 
-winners ballotSheets =
-                let loserCandidate = loser ballotSheets
-                in
-                map (delete loserCandidate) ballotSheets
+winners ballotSheets = (majorityCandidate $ topRankers ballotSheets) <|> winners ((map (delete (loser ballotSheets)) ballotSheets))
 
 loser ballotSheets = 
                 let candidates = head ballotSheets
-                    topRankers = map head ballotSheets
                 in
-                minimumBy (compare `on` (countIn topRankers)) candidates
+                lowestRankedCandidateDetail candidates (topRankers ballotSheets)
 
-countIn :: (Eq a) => [a] -> a -> Int
 countIn xs x = length (filter (== x) xs)
+
+topRankers ballotSheets = map head ballotSheets
 
 main = do
    putStrLn $ show $ winners vm
+
+lowestRankedCandidateDetail candidates topRankers = minimumBy (compare `on` (countIn topRankers)) candidates
+
+majorityCandidate topRanks = join $ listToMaybe <$> (find (\x -> (length x) > (length topRanks) `div` 2) $ group . sort $ topRanks)
+
+
