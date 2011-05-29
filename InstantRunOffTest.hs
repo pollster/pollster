ba96abe0c539cf19307ff058af86780dd635de48
Usha/Gavri <usha/gavri@example.com>
2011-05-30 00:13:15 +0530

    Majority criteria

--- InstantRunOffTest.hs
+++ InstantRunOffTest.hs
@@ -5,12 +5,25 @@ import Control.Monad.Trans
 import Control.Monad
 import Data.List
 
-prop_Simple = forAll ballotSheetsSimple $ (\xss -> (((length . join) xss) /= 0) ==> winner losersOneAtATime xss `elem` join xss)
+prop_Simple = forAll ballotSheetsSimple $ (\xss -> (not . null . join) xss ==> winner losersOneAtATime xss `elem` join xss)
+prop_MajorityCriteria = forAll ballotSheetsMajorityCriteria $ (\xss -> (not . null . join) xss  ==> winner losersOneAtATime (map ((:[]) . head) (filter (not . null) xss)) == winner losersOneAtATime xss)
 
 ballotSheetsSimple :: Gen [[Int]]
 ballotSheetsSimple = do
       res <- arbitrary
       return $ (fmap $ (nub . (fmap (`mod` 5)))) res
 
+ballotSheetsMajorityCriteria :: Gen [[Int]]
+ballotSheetsMajorityCriteria = do
+      majorityCandidate <- arbitrary
+      arbitraryList <- fmap (fmap (fmap (`mod` 5)) ) arbitrary
+      majority <- elements [smallestPossibleMajority(length arbitraryList)..length arbitraryList]
+      return $ fmap nub (zipWith ($) ((replicate majority ((majorityCandidate `mod` 5):)) ++ (repeat id)) arbitraryList)
+
+smallestPossibleMajority total
+                           | total == 0 = 0
+                           | otherwise = (total `div` 2) + 1
+
 main = do
       quickCheck prop_Simple
+      quickCheck prop_MajorityCriteria
